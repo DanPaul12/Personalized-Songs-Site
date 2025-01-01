f3d1eef724b33d84f2a8d8c3c042fde89beb7ba9
DanPaul12 <dan.paul.schechter@gmail.com>
2024-12-31 20:10:29 -0800

    complete basic setup

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["http://localhost:5173"])
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
