157a6eac0abcbcda30838de9ea420afeb168e907
DanPaul12 <dan.paul.schechter@gmail.com>
2025-02-07 20:57:02 -0800

    image minification

--- frontend/frontend-app/vite.config.js
+++ frontend/frontend-app/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import imagemin from 'vite-plugin-imagemin';
+
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    imagemin({
+      gifsicle: { optimizationLevel: 3 },
+      optipng: { optimizationLevel: 5 },
+      mozjpeg: { quality: 80 },
+      webp: { quality: 75 },
+    })
+  ],
 })
